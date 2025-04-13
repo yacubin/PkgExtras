a51141c2aae6ef46fdbbce6a0ccab64f183c8b95
Yurii Yakubin <yurii.yakubin@gmail.com>
2025-04-13 15:51:51 +0100

    BitMake script for nlohmann

---
+++ MakeScripts/nlohmann.mjs
@@ -0,0 +1,21 @@
+// RUN wget https://raw.githubusercontent.com/ykbin/PkgExtras/refs/heads/develop/MakeScripts/nlohmann.mjs
+// RUN npm init -y
+// RUN npm install bitmake
+// RUN npx bitmake build --config nlohmann.mjs
+
+export default {
+  "bundle:nlohmann": {
+    sourceUrl: "https://github.com/nlohmann/json/archive/refs/tags/v3.12.0.tar.gz",
+    action: "cmake",
+    generator: "Unix Makefiles",
+    cacheVariables: {
+      CMAKE_INSTALL_PREFIX: "/usr",
+      BUILD_TESTING: false,
+      JSON_BuildTests: false,
+      JSON_FastTests: false,
+      JSON_Install: true,
+    },
+    buildType: "Release",
+    destDir: "${binaryRoot}/host",
+  },
+};
