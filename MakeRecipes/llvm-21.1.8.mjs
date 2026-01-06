e61a3c632ffa1724873299f7546732dc5d62c6cc
Yurii Yakubin <yurii.yakubin@gmail.com>
2026-01-06 17:12:01 +0000

    llvm-21.1.8 recipe

---
+++ MakeRecipes/llvm-21.1.8.mjs
@@ -0,0 +1,20 @@
+// RUN wget https://raw.githubusercontent.com/yacubin/PkgExtras/refs/heads/develop/MakeRecipes/llvm-21.1.8.mjs
+// RUN npm init -y
+// RUN npm install bitmake
+// RUN npx bitmake build --config llvm-21.1.8.mjs
+
+export default {
+  "bundle:llvm": {
+    sourceUrl: "https://github.com/llvm/llvm-project/releases/download/llvmorg-21.1.8/llvm-project-21.1.8.src.tar.xz",
+    action: "cmake",
+    generator: "Unix Makefiles",
+    cacheVariables: {
+      CMAKE_INSTALL_PREFIX: "/usr",
+      LLVM_ENABLE_PROJECTS: [ "clang", "lld" ],
+      LLVM_TARGETS_TO_BUILD: [ "WebAssembly", "X86" ],
+    },
+    sourceDir: "./llvm",
+    buildType: "Release",
+    destDir: "${binaryRoot}/host",
+  },
+};
