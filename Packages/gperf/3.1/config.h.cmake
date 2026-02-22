3d7e2ec9a32ad7059b769f934c82cf80c39899b4
Yurii Yakubin <yurii.yakubin@gmail.com>
2026-02-22 22:50:41 +0000

    CMake build for bzip2-1.0.8 and gperf-3.1

---
+++ Packages/gperf/3.1/config.h.cmake
@@ -0,0 +1,25 @@
+/* config.h.cmake. Generated from configure.ac by autoheader. */
+
+/* Define to 1 if you have the <libintl.h> header file. */
+#cmakedefine HAVE_LIBINTL_H 1
+
+/* Define if the C++ compiler supports stack-allocated variable-size arrays. */
+#cmakedefine HAVE_DYNAMIC_ARRAY 1
+
+/* Define to the address where bug reports for this package should be sent. */
+#cmakedefine PACKAGE_BUGREPORT "@PACKAGE_BUGREPORT@"
+
+/* Define to the full name of this package. */
+#cmakedefine PACKAGE_NAME "@PACKAGE_NAME@"
+
+/* Define to the full name and version of this package. */
+#cmakedefine PACKAGE_STRING "@PACKAGE_STRING@"
+
+/* Define to the one symbol short name of this package. */
+#cmakedefine PACKAGE_TARNAME "@PACKAGE_TARNAME@"
+
+/* Define to the home page for this package. */
+#cmakedefine PACKAGE_URL "@PACKAGE_URL@"
+
+/* Define to the version of this package. */
+#cmakedefine PACKAGE_VERSION "@PACKAGE_VERSION@"
